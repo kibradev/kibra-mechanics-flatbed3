64c7490f5593b5adcbb6752a4a5f538f4c7c5b3a
kibra <62299912+kibradev@users.noreply.github.com>
2022-12-10 14:21:41 +0300

    for kibra-mechanics

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "bodacious"
+
+game "gta5"
+
+Author "Kibra#9999"
+
+files {
+    'data/carcols.meta',
+    'data/carvariations.meta',
+    'data/vehicles.meta',
+    'data/handling.meta',
+    'data/vehiclelayouts.meta',
+    'stream/def_flatbed3_props.ytyp',
+}
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+data_file 'DLC_ITYP_REQUEST' 'stream/def_flatbed3_props.ytyp'
